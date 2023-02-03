691ae142351e49730bc077baafaf1851e602771a
Cristian Alvear <cfam2212@gmail.com>
2023-02-03 18:36:14 -0500

    cambios realizados

--- expert_plus/pages/agregar_perfil.php
+++ expert_plus/pages/agregar_perfil.php
@@ -47,52 +47,46 @@
 
         <!--begin::Container-->
                 <div id="kt_content_container" class="container-xxl">
-                    <div class="flex-lg-row-fluid ms-lg-15">
-                        <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-bold mb-8">
-                            <li class="nav-item">
-                                <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab" href="#kt_ecommerce_settings_general">											
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path d="M16.0173 9H15.3945C14.2833 9 13.263 9.61425 12.7431 10.5963L12.154 11.7091C12.0645 11.8781 12.1072 12.0868 12.2559 12.2071L12.6402 12.5183C13.2631 13.0225 13.7556 13.6691 14.0764 14.4035L14.2321 14.7601C14.2957 14.9058 14.4396 15 14.5987 15H18.6747C19.7297 15 20.4057 13.8774 19.912 12.945L18.6686 10.5963C18.1487 9.61425 17.1285 9 16.0173 9Z" fill="currentColor" />
-                                        <rect opacity="0.3" x="14" y="4" width="4" height="4" rx="2" fill="currentColor" />
-                                        <path d="M4.65486 14.8559C5.40389 13.1224 7.11161 12 9 12C10.8884 12 12.5961 13.1224 13.3451 14.8559L14.793 18.2067C15.3636 19.5271 14.3955 21 12.9571 21H5.04292C3.60453 21 2.63644 19.5271 3.20698 18.2067L4.65486 14.8559Z" fill="currentColor" />
-                                        <rect opacity="0.3" x="6" y="5" width="6" height="6" rx="3" fill="currentColor" />
-                                    </svg>
-                                Perfil</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#kt_ecommerce_settings_store">
-                                    <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm004.svg-->
-                                    <span class="svg-icon svg-icon-2 me-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <path opacity="0.3" d="M18 10V20C18 20.6 18.4 21 19 21C19.6 21 20 20.6 20 20V10H18Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M11 10V17H6V10H4V20C4 20.6 4.4 21 5 21H12C12.6 21 13 20.6 13 20V10H11Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M10 10C10 11.1 9.1 12 8 12C6.9 12 6 11.1 6 10H10Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M18 10C18 11.1 17.1 12 16 12C14.9 12 14 11.1 14 10H18Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M14 4H10V10H14V4Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M17 4H20L22 10H18L17 4Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M7 4H4L2 10H6L7 4Z" fill="currentColor" />
-                                            <path d="M6 10C6 11.1 5.1 12 4 12C2.9 12 2 11.1 2 10H6ZM10 10C10 11.1 10.9 12 12 12C13.1 12 14 11.1 14 10H10ZM18 10C18 11.1 18.9 12 20 12C21.1 12 22 11.1 22 10H18ZM19 2H5C4.4 2 4 2.4 4 3V4H20V3C20 2.4 19.6 2 19 2ZM12 17C12 16.4 11.6 16 11 16H6C5.4 16 5 16.4 5 17C5 17.6 5.4 18 6 18H11C11.6 18 12 17.6 12 17Z" fill="currentColor" />
+                    <div class="card card-flush">
+                        <div class="card-header">
+                            <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-bold mb-8">
+                                <li class="nav-item">
+                                    <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab" href="#kt_ecommerce_settings_general">											
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path d="M16.0173 9H15.3945C14.2833 9 13.263 9.61425 12.7431 10.5963L12.154 11.7091C12.0645 11.8781 12.1072 12.0868 12.2559 12.2071L12.6402 12.5183C13.2631 13.0225 13.7556 13.6691 14.0764 14.4035L14.2321 14.7601C14.2957 14.9058 14.4396 15 14.5987 15H18.6747C19.7297 15 20.4057 13.8774 19.912 12.945L18.6686 10.5963C18.1487 9.61425 17.1285 9 16.0173 9Z" fill="currentColor" />
+                                            <rect opacity="0.3" x="14" y="4" width="4" height="4" rx="2" fill="currentColor" />
+                                            <path d="M4.65486 14.8559C5.40389 13.1224 7.11161 12 9 12C10.8884 12 12.5961 13.1224 13.3451 14.8559L14.793 18.2067C15.3636 19.5271 14.3955 21 12.9571 21H5.04292C3.60453 21 2.63644 19.5271 3.20698 18.2067L4.65486 14.8559Z" fill="currentColor" />
+                                            <rect opacity="0.3" x="6" y="5" width="6" height="6" rx="3" fill="currentColor" />
                                         </svg>
-                                    </span>
-                                Opciones Perfil</a>
-                            </li>
-                        </ul>
-                        <form id="kt_ecommerce_settings_general_form" class="form" action="">                                               
+                                    Perfil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#kt_ecommerce_settings_store">
+                                        <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm004.svg-->
+                                        <span class="svg-icon svg-icon-2 me-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                <path opacity="0.3" d="M18 10V20C18 20.6 18.4 21 19 21C19.6 21 20 20.6 20 20V10H18Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M11 10V17H6V10H4V20C4 20.6 4.4 21 5 21H12C12.6 21 13 20.6 13 20V10H11Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M10 10C10 11.1 9.1 12 8 12C6.9 12 6 11.1 6 10H10Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M18 10C18 11.1 17.1 12 16 12C14.9 12 14 11.1 14 10H18Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M14 4H10V10H14V4Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M17 4H20L22 10H18L17 4Z" fill="currentColor" />
+                                                <path opacity="0.3" d="M7 4H4L2 10H6L7 4Z" fill="currentColor" />
+                                                <path d="M6 10C6 11.1 5.1 12 4 12C2.9 12 2 11.1 2 10H6ZM10 10C10 11.1 10.9 12 12 12C13.1 12 14 11.1 14 10H10ZM18 10C18 11.1 18.9 12 20 12C21.1 12 22 11.1 22 10H18ZM19 2H5C4.4 2 4 2.4 4 3V4H20V3C20 2.4 19.6 2 19 2ZM12 17C12 16.4 11.6 16 11 16H6C5.4 16 5 16.4 5 17C5 17.6 5.4 18 6 18H11C11.6 18 12 17.6 12 17Z" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                    Opciones Perfil</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <form class="form mb-15" method="post" enctype='multipart/form-data'>                                               
                             <div class="tab-content" id="myTabContent">  
-                                <div class="card card-flush">  
                                     <div class="card-header"> 
                                         <div class="card-toolbar">
-                                            <button type="submit" class="btn btn-light-primary"><i class="las la-save"></i>Guardar</button>
+                                            <button type="button" id="btnGuardar" class="btn btn-light-primary"><i class="las la-save"></i>Guardar</button>
                                         </div>
-                                    </div>    
-                                </div>                                                                                
-                                <div class="tab-pane fade show active" id="kt_ecommerce_settings_general" role="tabpanel">
-                                    <div class="card card-flush">                                                      
-                                        <div class="card-header align-items-center py-5 gap-2 gap-md-5">
-                                            <div class="card-title">
-                                                <h2>Perfil</h2>
-                                            </div>
-                                        </div>
+                                    </div>                                                                                
+                                <div class="tab-pane fade show active" id="kt_ecommerce_settings_general" role="tabpanel">                                                                                        
                                         <div class="card-body pt-0">													
                                             <div class="row fv-row mb-7">
                                                 <div class="col-md-3 text-md-end">
@@ -142,7 +136,7 @@
                                                 </div>
                                             </div>                                                        
                                         </div>
-                                    </div>
+                                    
                                 </div>
                                 <div class="tab-pane fade" id="kt_ecommerce_settings_store" role="tabpanel">
                                     <div class="card card-flush">
@@ -177,11 +171,10 @@
                                                     <?php
                                                         foreach ($all_perfiles as $perfil){    
                                                     ?>
-                                                    <tr>
+                                                    <tr id="tr_<?php echo $perfil['MentId']; ?>">
                                                         <td style="text-align:center">
                                                             <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                                                <input class="form-check-input" type="checkbox" <?php echo $chkEstado; ?> id="chk<?php echo $perfil['MentId']; ?>" <?php if ($perfil['Estado'] == 'Activo') {
-                                                                    echo "checked";} else {'';} ?> value="<?php echo $perfil['MentId']; ?>" />
+                                                                <input class="form-check-input" type="checkbox" name="chkMenu" id="chkMenu" value="<?php echo $perfil['MentId']; ?>" />
                                                             </div>
                                                         </td>                                                                      
                                                         <td><?php echo $perfil['Menu']; ?></td>
@@ -197,5 +190,72 @@
                                 </div>                                                                             
                             </div>
                         </form>   
+                        
                     </div>
                 </div>
+                
+                <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+                <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+
+            <script>
+
+                $(document).ready(function(){
+
+                    $(document).on("click","#chkMenu",function(){
+                        let _rowid = $(this).attr("id");
+                        alert(_rowid);
+                    });
+                    
+                    $('#btnGuardar').click(function(){
+                        _perfil = $.trim($("#txtPerfil").val());
+                        _observacion = $.trim($("#txtDescripcion").val());
+                        _estado = "Activo";
+
+                        alert(_perfil);
+
+                        // if(_perfil == '')
+                        // {       
+
+                        //     mensajesalertify("Ingrese Nombre del Perfil..!!","W","top-right",3);  
+                        //     return;
+                        // }
+
+                        // var i = 0;
+
+                        // $("input[type=checkbox]:checked").map(function(){
+                        //     if($(this).val() != 'on'){
+                        //         _result[i] = $(this).val();
+                        //         i++;
+                        //     }
+                        // });
+
+                        // if(i == 0)
+                        // {
+                        
+                        //     mensajesalertify("Seleccione al menos un opción Menu/Tareal..!!","W","top-right",3);
+                        //     return;
+                        // }
+
+                        // $.ajax({
+                        //     url: "../db/consultadatos.php",
+                        //     type: "POST",
+                        //     dataType: "json",
+                        //     data: {tipo:14, auxv1:"", auxv2:_perfil, auxv3:"", auxv4:"", auxv5:"", auxv6:"", auxi1:0, auxi2:0, auxi3:0, auxi4:0, auxi5:0, auxi6:0, 
+                        //     opcion:0},
+                        //     success: function(data){                    
+                        //         if(data[0].contar == "0"){                         
+                        //             _continuar = true;
+                        //         }else{                      
+                        //             _continuar = false;
+                        //         }   
+                        //         FunGrabar(_continuar);
+                        //     },
+                        //     error: function (error) {
+                        //         console.log(error);
+                        //     }
+                        // });
+                    });                     
+
+
+                }); 
+            </script> 
