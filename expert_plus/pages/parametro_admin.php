9e09e9a70df4026a7f5d0e967a9f9f9cee3df9ac
Cristian Alvear <cfam2212@gmail.com>
2023-03-21 20:09:44 -0500

    cr

--- expert_plus/pages/parametro_admin.php
+++ expert_plus/pages/parametro_admin.php
@@ -38,42 +38,32 @@
 ?>
 
 <div id="kt_content_container" class="container-xxl">
-	<div class="card card-flush">
-		<div class="card-toolbar">
-			<button class="btn btn-sm btn-light-primary" id="nuevoParametro">
-                <span class="svg-icon svg-icon-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor" />
-                        <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="currentColor" />
-                    </svg>
-                </span>
-                Nuevo Parametro
-            </button>
-		</div>
-		<div class="card-header align-items-center py-5 gap-2 gap-md-5">
-			<div class="card-title">
-				<div class="d-flex align-items-center position-relative my-1">
-					<span class="svg-icon svg-icon-1 position-absolute ms-4">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-							<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
-							<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
-						</svg>
-					</span>
-					<input type="text" data-kt-ecommerce-order-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Buscar Dato" />
-				</div>
-				<div id="kt_ecommerce_report_shipping_export" class="d-none"></div>
-			</div>
-			<div class="card-toolbar flex-row-fluid justify-content-end gap-5">
-				<div class="w-150px">
-					<select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Estado" data-kt-ecommerce-order-filter="status">
-						<option></option>
-						<option value="all">Todos</option>
-						<option value="Activo">Activo</option>
-						<option value="Inactivo">Inactivo</option>
-					</select>
-				</div>
-			</div>
-		</div>
+
+    <div class="card">
+        <div class="card-header border-0 pt-6">
+            <div class="card-title">
+                <div class="d-flex align-items-center position-relative my-1">
+                    <span class="svg-icon svg-icon-1 position-absolute ms-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
+                            <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
+                        </svg>
+                    </span>
+                    <input type="text" data-kt-ecommerce-order-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Buscar Dato" />
+                </div>
+            </div> 
+            <div class="card-toolbar">
+                <button class="btn btn-primary" id="nuevoParametro">
+                    <span class="svg-icon svg-icon-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor" />
+                            <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="currentColor" />
+                        </svg>
+                    </span>
+                    Nuevo Parametro
+                </button>
+		    </div>                       
+        </div>
 		<div class="card-body pt-0">
 			<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_report_shipping_table" style="width: 100%;">
 				<thead>
@@ -136,6 +126,7 @@
 		</div>
 	</div>
 </div>
+
 <div class="modal fade" id="modal_parametro" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
