d9a50fa226d9a9d319b68baa32ec21e86e7f961c
Erick <erick.alvear7@gmail.com>
2023-10-10 12:00:48 -0500

    changev2

--- expert_plus/pages/editar_titular.php
+++ expert_plus/pages/editar_titular.php
@@ -248,7 +248,7 @@
             </ul>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="kt_user_view_overview_tab" role="tabpanel">
-                    <div class="card card-flush py-4">
+                    <div class="card pt-4 mb-6 mb-xl-9">
                         <div class="card-header">
                             <div class="card-title">
                                 <h2>Datos Beneficiario</h2>
@@ -386,8 +386,6 @@
                             </div>  
                         </div>
                     </div>
-                    <div class="separator"></div>
-                    <br>
                     <div class="card pt-4 mb-6 mb-xl-9">                    
                         <div class="card-header border-0">                         
                             <div class="card-title">
@@ -483,47 +481,7 @@
                                 </table>
                             </div>
                         </div>
-                    </div>
-                    <div class="card pt-4 mb-6 mb-xl-9">
-                        <div class="card-header border-0">
-                            <div class="card-title">
-                                <h2>Logs</h2>
-                            </div>
-                            <div class="card-toolbar">
-                                <button type="button" class="btn btn-sm btn-light-primary">
-                                <span class="svg-icon svg-icon-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path opacity="0.3" d="M19 15C20.7 15 22 13.7 22 12C22 10.3 20.7 9 19 9C18.9 9 18.9 9 18.8 9C18.9 8.7 19 8.3 19 8C19 6.3 17.7 5 16 5C15.4 5 14.8 5.2 14.3 5.5C13.4 4 11.8 3 10 3C7.2 3 5 5.2 5 8C5 8.3 5 8.7 5.1 9H5C3.3 9 2 10.3 2 12C2 13.7 3.3 15 5 15H19Z" fill="currentColor" />
-                                        <path d="M13 17.4V12C13 11.4 12.6 11 12 11C11.4 11 11 11.4 11 12V17.4H13Z" fill="currentColor" />
-                                        <path opacity="0.3" d="M8 17.4H16L12.7 20.7C12.3 21.1 11.7 21.1 11.3 20.7L8 17.4Z" fill="currentColor" />
-                                    </svg>
-                                </span>
-                                Download Report</button>
-                            </div>
-                        </div>
-                        <div class="card-body py-0">
-                            <div class="table-responsive">
-                                <table class="table align-middle table-row-dashed fw-bold text-gray-600 fs-6 gy-5" id="kt_table_users_logs">
-                                    <tbody>
-                                        <tr>
-                                            <td class="min-w-70px">
-                                                <div class="badge badge-light-danger">500 ERR</div>
-                                            </td>
-                                            <td>POST /v1/invoice/in_5315_4014/invalid</td>
-                                            <td class="pe-0 text-end min-w-200px">10 Nov 2022, 10:30 am</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="min-w-70px">
-                                                <div class="badge badge-light-success">200 OK</div>
-                                            </td>
-                                            <td>POST /v1/invoices/in_7445_4506/payment</td>
-                                            <td class="pe-0 text-end min-w-200px">25 Jul 2022, 11:30 am</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>            
+                    </div>           
                 </div>
             </div>
         </div>
