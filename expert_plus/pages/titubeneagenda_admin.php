a30ee09629aa2cda43256452eed8d9b3391886da
Erick <erick.alvear7@gmail.com>
2024-05-16 13:26:06 -0500

    correeccionesv42

--- expert_plus/pages/titubeneagenda_admin.php
+++ expert_plus/pages/titubeneagenda_admin.php
@@ -508,15 +508,8 @@
                         </div>                                
                     </div> 
                     <div class="card-toolbar">
-                        <button class="btn btn-sm btn-flex btn-light-primary" id="btnCalendar">
-                            <span class="svg-icon svg-icon-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="currentColor" />
-                                    <rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
-                                    <rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="currentColor" />
-                                </svg>
-                            </span>
-                            Nuevo Agendamiento
+                        <button type="button" data-repeater-create="" class="btn btn-light-primary btn-sm mb-2" id="btnCalendar"><i class="fa fa-plus-circle" aria-hidden="true"></i>
+                           Nuevo Agendamiento
                         </button>
                     </div>
                 </div>
