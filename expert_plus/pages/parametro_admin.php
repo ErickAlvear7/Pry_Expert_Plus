e424809e38a4137c9ca751938999a88fc5abbdf4
Erick <erick.alvear7@gmail.com>
2024-06-11 12:59:40 -0500

    correc_paramv1

--- expert_plus/pages/parametro_admin.php
+++ expert_plus/pages/parametro_admin.php
@@ -66,8 +66,8 @@
 				<thead>
 					<tr class="text-start text-gray-800 fw-bolder fs-7 text-uppercase gs-0">
 					    <th style="display:none;">Id</th>
-						<th class="min-w-125px">Parametro</th>
-						<th class="min-w-125px">Descripcion</th>
+						<th class="">Parametro</th>
+						<th class="">Descripcion</th>
 						<th class="min-w-125px">Estado</th>
 						<th>Status</th>
                         <th style="text-align: center;">Opciones</th>
@@ -143,36 +143,21 @@
                     </span>
                 </div>
             </div>
-            <div class="modal-body py-lg-10 px-lg-10">
+            <div class="modal-body py-lg-7 px-lg-10">
                 <div class="card card-flush py-4">
-                    <div class="card-header">
+                    <div class="card-header mb-7">
                         <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-bold mb-8">
                             <li class="nav-item">
                                 <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab" href="#kt_ecommerce_settings_general">											
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path d="M16.0173 9H15.3945C14.2833 9 13.263 9.61425 12.7431 10.5963L12.154 11.7091C12.0645 11.8781 12.1072 12.0868 12.2559 12.2071L12.6402 12.5183C13.2631 13.0225 13.7556 13.6691 14.0764 14.4035L14.2321 14.7601C14.2957 14.9058 14.4396 15 14.5987 15H18.6747C19.7297 15 20.4057 13.8774 19.912 12.945L18.6686 10.5963C18.1487 9.61425 17.1285 9 16.0173 9Z" fill="currentColor" />
-                                        <rect opacity="0.3" x="14" y="4" width="4" height="4" rx="2" fill="currentColor" />
-                                        <path d="M4.65486 14.8559C5.40389 13.1224 7.11161 12 9 12C10.8884 12 12.5961 13.1224 13.3451 14.8559L14.793 18.2067C15.3636 19.5271 14.3955 21 12.9571 21H5.04292C3.60453 21 2.63644 19.5271 3.20698 18.2067L4.65486 14.8559Z" fill="currentColor" />
-                                        <rect opacity="0.3" x="6" y="5" width="6" height="6" rx="3" fill="currentColor" />
-                                    </svg>
-                                Parametro</a>
+                                    <i class="fa fa-tasks fa-1x me-2" aria-hidden="true"></i>
+                                    Parametro
+                                </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#kt_ecommerce_settings_store">
-                                    <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm004.svg-->
-                                    <span class="svg-icon svg-icon-2 me-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <path opacity="0.3" d="M18 10V20C18 20.6 18.4 21 19 21C19.6 21 20 20.6 20 20V10H18Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M11 10V17H6V10H4V20C4 20.6 4.4 21 5 21H12C12.6 21 13 20.6 13 20V10H11Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M10 10C10 11.1 9.1 12 8 12C6.9 12 6 11.1 6 10H10Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M18 10C18 11.1 17.1 12 16 12C14.9 12 14 11.1 14 10H18Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M14 4H10V10H14V4Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M17 4H20L22 10H18L17 4Z" fill="currentColor" />
-                                            <path opacity="0.3" d="M7 4H4L2 10H6L7 4Z" fill="currentColor" />
-                                            <path d="M6 10C6 11.1 5.1 12 4 12C2.9 12 2 11.1 2 10H6ZM10 10C10 11.1 10.9 12 12 12C13.1 12 14 11.1 14 10H10ZM18 10C18 11.1 18.9 12 20 12C21.1 12 22 11.1 22 10H18ZM19 2H5C4.4 2 4 2.4 4 3V4H20V3C20 2.4 19.6 2 19 2ZM12 17C12 16.4 11.6 16 11 16H6C5.4 16 5 16.4 5 17C5 17.6 5.4 18 6 18H11C11.6 18 12 17.6 12 17Z" fill="currentColor" />
-                                        </svg>
-                                    </span>
-                                Detalle</a>
+                                    <i class="fa fa-sitemap fa-1x me-2" aria-hidden="true"></i>
+                                    Detalle
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -180,22 +165,22 @@
                         <div class="tab-content" id="myTabContent">
                             <div class="tab-pane fade show active" id="kt_ecommerce_settings_general" role="tabpanel"> 
                                 <div class="card-body pt-0">
-                                    <div class="row g-9 mb-7">
+                                    <div class="row g-9 mb-10">
                                         <div class="col-md-12 fv-row">
-                                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                            <label class="d-flex align-items-center fs-6 fw-bold mb-3">
                                                 <span class="required">Parametro</span>
-                                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="especifique el nombre del parametro"></i>
+                                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Especifique el nombre del parametro"></i>
                                             </label>
-                                            <input type="text" class="form-control form-control-solid" id="txtNombrePara" name="txtNombrePara" minlength="5" maxlength="100" placeholder="Ingrese Nombre del Parametro" value="" />
+                                            <input type="text" class="form-control" id="txtNombrePara" name="txtNombrePara" minlength="5" maxlength="100" placeholder="Ingrese Parametro" value="" />
                                         </div>
                                     </div>
-                                    <div class="row g-9 mb-7">
+                                    <div class="row g-9 mb-2">
                                         <div class="col-md-12 fv-row">
-                                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                            <label class="d-flex align-items-center fs-6 fw-bold mb-3">
                                                 <span class="required">Descripcion</span>
-                                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="especifique una descripcion del parametro"></i>
+                                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Especifique una descripcion del parametro"></i>
                                             </label>
-                                            <textarea class="form-control form-control-solid" name="txtDesc" id="txtDesc" maxlength="150" onkeydown="return (event.keyCode!=13);"></textarea>
+                                            <textarea class="form-control" name="txtDesc" id="txtDesc" maxlength="150" onkeydown="return (event.keyCode!=13);"></textarea>
                                         </div>
                                     </div>
                                 </div>
@@ -207,21 +192,21 @@
                                         <div class="col-md-4 fv-row">
                                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
                                             <span class="required">Detalle</span>
-                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="especifique el nombre del detalle"></i>
+                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Especifique el nombre del detalle"></i>
                                             </label>
-                                            <input type="text" class="form-control form-control-solid" id="txtDetalle" name="txtDetalle" minlength="2" maxlength="100" placeholder="nombre del detalle" value="" />                       
+                                            <input type="text" class="form-control" id="txtDetalle" name="txtDetalle" minlength="2" maxlength="100" placeholder="Ingrese Detalle" value="" />                       
                                         </div>
                                         <div class="col-md-3 fv-row">
                                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
                                             <span class="required">Valor Texto</span>
-                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="solo valor en texto"></i>
+                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Solo valores en texto"></i>
                                             </label>
-                                            <input type="text" class="form-control form-control-solid" id="txtValorV" name="txtValorV" minlength="1" maxlength="50" placeholder="valor texto" value="" />                       
+                                            <input type="text" class="form-control" id="txtValorV" name="txtValorV" minlength="1" maxlength="50" placeholder="valor texto" value="" />                       
                                         </div>
                                         <div class="col-md-3 fv-row">
                                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
                                             <span class="required">Valor Entero</span>
-                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="solo valores enteros"></i>
+                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Solo valores enteros"></i>
                                             </label>
                                             <input type="text" class="form-control form-control-solid" id="txtValorI" name="txtValorI" onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;" minlength="1" maxlength="10" placeholder="valor entero" value="" />                       
                                         </div>
@@ -239,9 +224,9 @@
                                             <tr class="text-start text-gray-800 fw-bolder fs-7 text-uppercase gs-0">
                                                 <th style="display:none;">Id</th>
                                                 <th class="min-w-125px">Detalle</th>
-                                                <th class="min-w-125px">Valor Texto</th>
-                                                <th class="min-w-125px">Valor entero</th>
-                                                <th class="min-w-125px" style="text-align: center;">Opciones</th>
+                                                <th>Valor Texto</th>
+                                                <th>Valor entero</th>
+                                                <th style="text-align: center;">Opciones</th>
                                             </tr>
                                         </thead>
                                         <tbody class="fw-bold text-gray-600"></tbody>
@@ -253,7 +238,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" id="closemodal" class="btn btn-sm btn-light-danger" data-bs-dismiss="modal"><i class="fa fa-window-close" aria-hidden="true"></i>Cerrar</button>
+                <button type="button" class="btn btn-sm btn-light-danger" data-bs-dismiss="modal"><i class="fa fa-window-close" aria-hidden="true"></i>Cerrar</button>
                 <button type="button" id="btnGuardar" class="btn btn-sm btn-light-primary" onclick="f_Guardar(<?php echo $xPaisid; ?>,<?php echo $xEmprid; ?>,<?php echo $xUsuaid; ?>)"><i class="las la-save"></i>Grabar</button>
             </div>
         </div>
@@ -293,7 +278,7 @@
         });  
     });
 
-    //Agregar detalle del paametro
+    //Agregar detalle del parametro modal
 
     $('#btnAgregar').click(function(){
       //debugger;
@@ -311,13 +296,13 @@
 
         if($.trim($('#txtValorV').val()).length == 0 && $.trim($('#txtValorI').val()).length == 0 )
         {    
-            toastSweetAlert("top-end",3000,"warning","Ingrese Valor Texto o Entero..!");
+            toastSweetAlert("top-end",3000,"warning","Ingrese valor texto o entero..!");
             return false;
         }
 
         if($.trim($('#txtValorV').val()).length > 0 && $.trim($('#txtValorI').val()).length > 0 )
         {    
-            toastSweetAlert("top-end",3000,"warning","Solo Valor Texto o Entero..!!");
+            toastSweetAlert("top-end",3000,"warning","Solo valor texto o entero..!!");
             return false;
         }
 
@@ -347,7 +332,7 @@
                     $.each(_result,function(i,item){
                         if(item.arrydetalle.toUpperCase() == _detalle.toUpperCase())
                         {                  
-                            toastSweetAlert("top-end",3000,"warning","Detalle ya Existe..!!");
+                            toastSweetAlert("top-end",3000,"warning","Detalle ya existe..!!");
                             _continuar = false;
                             return false;
                         }else{
@@ -356,7 +341,7 @@
                                 {
                                     if(item.arryvalorv.toUpperCase() == _valorV.toUpperCase())
                                     {                               
-                                        toastSweetAlert("top-end",3000,"warning","Valor Texto ya Existe..!!");
+                                        toastSweetAlert("top-end",3000,"warning","Valor texto ya existe..!!");
                                         _continuar = false;
                                         return false;
                                     }else{
@@ -366,7 +351,7 @@
                                 {
                                     if(item.arryvalori == _valorI)
                                     {                               
-                                        toastSweetAlert("top-end",3000,"warning","Valor Entero ya Existe..!!");
+                                        toastSweetAlert("top-end",3000,"warning","Valor entero ya existe..!!");
                                         _continuar = false;
                                         return false;
                                     }else{
@@ -405,34 +390,13 @@
                         $("#txtValorI").val("");
                     }
                 }else{
-                    toastSweetAlert("top-end",3000,"warning","Detalle y/o Valor Texto o Entero ya existe..!!");
+                    toastSweetAlert("top-end",3000,"warning","Detalle valor texto o entero ya existe..!!");
                 }
             });
         }
     });
 
-    // $('#modal_parametro').on('hidden.bs.modal', function () {
-    //     debugger;
-
-    //     _result.length = 0;
-    //     console.log(_result);
-    // });
-
-    $('#closemodal').click(function() {
-
-        $.each(_result,function(i,item){
-         
-            console.log(item);
-            delete(item.arrydetalle);
-
-            console.log(item);
-
-        }); 
-
-      
-       
-    });
-
+  
    
    //Guardar parametro-detalle
 
@@ -514,7 +478,7 @@
                     }
                 }); 
             }else{
-                toastSweetAlert("top-end",3000,"warning","Parametro ya Existe..!!");
+                toastSweetAlert("top-end",3000,"warning","Parametro ya existe..!!");
             }
         });
     }
@@ -535,11 +499,8 @@
             }
         }); 
 
-        //FunRemoveItemFromArr(_result, _detalle);
         $('#row_' + row_id + '').remove();
         _count--;
-
-        console.log(_result);
 
     });
 
